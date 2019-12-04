991c58fc0983781ce0e0dbe59d96db3163c7c5e3
Adrien Genic <adrien@typhoon-hil.com>
2019-12-04 19:07:58 +0100

    Added contactor tests

---
+++ tests/test_psim/test_contactors/test_single_phase_contactor/test_test_single_phase_contactor.py
@@ -0,0 +1,96 @@
+# Imports
+import typhoon.api.hil as hil
+from tests.utils import psim_export_netxml
+from typhoon.api.schematic_editor import model
+from typhoon.test.capture import start_capture, get_capture_results
+import typhoon.test.signals as sig
+from typhoon.test.ranges import around
+import pytest
+import os
+from model_converter.converter.app.converter import Converter
+
+vhil = True
+
+# Define the paths
+psim_tests_dir = os.path.dirname(__file__)
+tests_dir = os.path.dirname(psim_tests_dir)
+sch_importer_dir = os.path.dirname(tests_dir)
+
+psimsch_path = psim_tests_dir + '\\single_phase_contactor.psimsch'
+
+
+@pytest.fixture(scope='session')
+def create_psim_netxml():
+    # Generates a xml netlist from psim schematic file
+    return psim_export_netxml(psimsch_path)
+
+
+@pytest.fixture(scope='session')
+def convert_xml2tse(create_psim_netxml):
+    # Converts the psim xml netlist to tse
+    netxml_path = create_psim_netxml
+    converter = Converter("psim", netxml_path)
+    tse_path = converter.convert_schema(compile_model=False)
+    return tse_path
+
+
+@pytest.fixture(scope='session')
+def convert_compile_load(convert_xml2tse):
+    tse_path = convert_xml2tse
+    cpd_path = tse_path[:-4] + " Target files\\" + tse_path.split("\\")[-1][:-4] + ".cpd"
+    # Open the converted tse file
+    model.load(tse_path)
+    # Compile the model
+    model.compile()
+
+    # Load to VHIL
+    hil.load_model(file=cpd_path, offlineMode=False, vhil_device=vhil)
+
+@pytest.mark.generate_netxml
+def test_generate_netxml(create_psim_netxml):
+    netxml_path = create_psim_netxml
+    assert os.path.isfile(netxml_path)
+
+
+@pytest.mark.conversion_xml2tse
+def test_conversion_xml2tse(convert_xml2tse):
+    tse_path = convert_xml2tse
+    assert os.path.isfile(tse_path)
+
+
+@pytest.mark.parametrize("Vss, f, ss_value, Iss_exp",
+                         [(220, 50, True, 22),
+                          (220, 50, False, 0)])
+def test_bi_directional_switch(convert_compile_load, Vss, f, ss_value, Iss_exp):
+
+    # Set source value
+    hil.set_source_sine_waveform(name='Vss', rms=Vss, frequency=f)
+
+    # Set switch state
+    hil.set_contactor('SS', swControl=True, swState=ss_value)
+
+    # Start capture
+    start_capture(duration=0.1, signals=['Iss'], executeAt=0.2)
+
+    # Start simulation
+    hil.start_simulation()
+
+    # Data acquisition
+    capture = get_capture_results(wait_capture=True)
+    Iss = capture['Iss']
+
+    # Tests
+    sig.assert_is_constant(Iss, at_value=around(Iss_exp, tol_p=0.01))
+
+    # Stop simulation
+    hil.stop_simulation()
+
+
+
+
+
+
+
+
+
+

c9b1026c95059088bb6d057d492a02f478f73393
Adrien Genic <adrien@typhoon-hil.com>
2020-02-19 16:29:25 +0100

    Added support for PV Panel. Added default value for Rm in transformers. Added test for PV Panel

---
+++ tests/test_psim/test_sources_measurements/test_pv_panel/test_pv_panel.py
@@ -0,0 +1,83 @@
+# Imports
+import typhoon.api.hil as hil
+import typhoon.test.signals as sig
+from tests.utils import psim_export_netxml
+from typhoon.test.ranges import around
+from typhoon.api.schematic_editor import model
+from typhoon.test.capture import start_capture, get_capture_results
+import pytest
+import os
+from model_converter.converter.app.converter import Converter
+
+
+vhil = True
+
+# Define the paths
+psim_tests_dir = os.path.dirname(__file__)
+tests_dir = os.path.dirname(psim_tests_dir)
+sch_importer_dir = os.path.dirname(tests_dir)
+
+psimsch_path = psim_tests_dir + '\\pv_panel.psimsch'
+
+
+@pytest.fixture(scope='session')
+def create_psim_netxml():
+    # Generates a xml netlist from psim schematic file
+    return psim_export_netxml(psimsch_path)
+
+
+@pytest.fixture(scope='session')
+def convert_xml2tse(create_psim_netxml):
+    # Converts the psim xml netlist to tse
+    netxml_path = create_psim_netxml[1]
+    converter = Converter("psim", netxml_path)
+    tse_path = converter.convert_schema(compile_model=False)[0]
+    return tse_path
+
+
+@pytest.fixture(scope='session')
+def convert_compile_load(convert_xml2tse):
+    tse_path = convert_xml2tse
+    cpd_path = tse_path[:-4] + " Target files\\" + tse_path.split("\\")[-1][:-4] + ".cpd"
+    # Open the converted tse file
+    model.load(tse_path)
+    # Compile the model
+    model.compile()
+
+    # Load to VHIL
+    hil.load_model(file=cpd_path, offlineMode=False, vhil_device=vhil)
+
+    hil.set_pv_input_file("SCP1", psim_tests_dir + '\\E50530_Thin_Film.ipvx', illumination=1000.0, temperature=25.0)
+
+    # Start simulation
+    hil.start_simulation()
+
+    yield
+
+    hil.stop_simulation()
+
+@pytest.mark.generate_netxml
+def test_generate_netxml(create_psim_netxml):
+    netxml_path = create_psim_netxml[1]
+    assert create_psim_netxml[0] == 0
+    assert os.path.isfile(netxml_path)
+
+
+@pytest.mark.conversion_xml2tse
+def test_conversion_xml2tse(convert_xml2tse):
+    tse_path = convert_xml2tse
+    assert os.path.isfile(tse_path)
+
+
+def test_pv_pvanel(convert_compile_load):
+
+    # Start capture
+    sim_time = hil.get_sim_time()
+    start_capture(duration=0.1, signals=["I1"], executeAt=sim_time + 0.5)
+
+    # Data acquisition
+    cap_data = get_capture_results(wait_capture=True)
+    measurement = cap_data
+
+    # Tests
+    sig.assert_is_constant(measurement["I1"], at_value=around(5.21, tol_p=0.001))

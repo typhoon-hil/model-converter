28ce8db3dea806120344f2dc56c85ba423754a49
pojzn <domic.aleksa@gmail.com>
2020-05-07 17:08:06 +0200

    Update setup.py
    Version bump in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model_converter',
-    version='1.4.3',
+    version='1.4.4',
     packages=find_packages(),
     install_requires=["parglare==0.10.0", "typhoon-hil-api"],
     url='https://www.typhoon-hil.com/',

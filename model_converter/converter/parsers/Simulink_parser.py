9c615ce38ffcbc474d879803930f45d84f08626d
aleksa.domic <aleksa.domic@typhoon-hil.com>
2021-07-07 13:38:20 +0200

    Added standard block property parsing.

--- model_converter/converter/parsers/Simulink_parser.py
+++ model_converter/converter/parsers/Simulink_parser.py
@@ -257,6 +257,10 @@ class SimulinkParser(BaseParser):
                 elif prop_name == "Side":
                     term_side = self._create_input_obj_model(child)
                     new_component.properties.update(term_side)
+                else:
+                    for prop in child:
+                        ret_val = self._create_input_obj_model(prop)
+                        new_component.properties.update(ret_val)
             #
             # The block is a subsystem element
             #
